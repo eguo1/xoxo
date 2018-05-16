0cb3d2b6b7d3a2d9130f5f0d47dc1c6cbf0197c5
Samir Thakral <samirthakral@icloud.com>
2018-05-16 14:45:14 -0500

    Finished streak

--- game/index.js
+++ game/index.js
@@ -3,10 +3,34 @@ import { createStore } from 'redux'
 
 let ticTacToe = Map();
 
-for(let i=0;i<3;i++){
-  for(let j=0;j<3;j++){
-    ticTacToe = ticTacToe.setIn([i,j],'_');
+function winner(){
+
+}
+
+function streak(board,...coords){
+  let coordStatus = {}
+  for(let i=0;i<coords.length;i++){
+    let coord = board.getIn(coords[i]);
+    if(coord === undefined)
+      return
+    if(Object.keys(coordStatus).includes(coord))
+      coordStatus[coord] += 1;
+    else
+      coordStatus[coord] = 1
   }
+  console.log(coordStatus);
+  if(coordStatus['X'] === 3)
+    return 'X'
+  else if(coordStatus['O'] === 3)
+    return 'O'
+  else
+    return
+  // let coordStatus;
+  // return coords.reduce((result,coord) => {
+  //   console.log('Reduce ran', board.getIn(coord));
+  //   if(!board.getIn(coord)) return
+  //   coordStatus = boar
+  // })
 }
 
 const initialState = {
@@ -19,8 +43,10 @@ const initialState = {
 export default function reducer(state=initialState, action) {
   if(action.type === 'MOVE') {
     const nextTurn = state.turn === 'X' ? 'O' : 'X'
+    const newBoard = state.board.setIn(action.position, state.turn);
+    console.log(streak(newBoard,[0,0],[0,1],[0,2]));
     return {
-      board: ticTacToe.setIn(action.position, state.turn),
+      board: newBoard,
       turn: nextTurn
     }
   }
