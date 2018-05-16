365d350fc83077264eaf1ee763ec81ab79ec3f24
Samir Thakral <samirthakral@icloud.com>
2018-05-16 11:28:12 -0500

    First Commit

--- game/index.js
+++ game/index.js
@@ -1,6 +1,27 @@
-import {Map} from 'immutable'
+const {Map} = require('immutable');
+
+let ticTacToe = Map();
+
+for(let i=0;i<3;i++){
+  for(let j=0;j<3;j++){
+    ticTacToe = ticTacToe.setIn([i,j],'_');
+  }
+}
+
+//console.log(ticTacToe);
 
 export default function reducer(state, action) {
   // TODO
   return state
 }
+
+//ACTION TYPES
+const Move = (position, player) => ({
+  type: 'MOVE',
+  position,
+  player
+})
+
+
+
+
