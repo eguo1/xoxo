541d06e3bc5afc64ac908406ab1129e66709e760
Eric Guo <ericxguo@gmail.com>
2018-05-16 11:58:40 -0500

    kinda working?

--- game/index.js
+++ game/index.js
@@ -1,4 +1,5 @@
-const {Map} = require('immutable');
+import { Map } from 'immutable';
+import { createStore } from 'redux'
 
 let ticTacToe = Map();
 
@@ -8,15 +9,26 @@ for(let i=0;i<3;i++){
   }
 }
 
+const initialState = {
+  board: ticTacToe,
+  turn: 'X'
+}
+
 //console.log(ticTacToe);
 
-export default function reducer(state, action) {
-  // TODO
+export default function reducer(state=initialState, action) {
+  if(action.type === 'MOVE') {
+    const nextTurn = state.turn === 'X' ? 'O' : 'X'
+    return {
+      board: ticTacToe.setIn(action.position, state.turn),
+      turn: nextTurn
+    }
+  }
   return state
 }
 
 //ACTION TYPES
-const Move = (position, player) => ({
+export const move = (player, position) => ({
   type: 'MOVE',
   position,
   player
